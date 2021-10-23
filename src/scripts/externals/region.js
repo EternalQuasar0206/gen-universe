2964d5372288ca917383c6136ce2fd9d19f55d5e
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-23 12:38:18 -0300

    Update region script (dynamic content block)

--- src/scripts/externals/region.js
+++ src/scripts/externals/region.js
@@ -1,3 +1,14 @@
 /* Replace first letter of region name to upper case */
 var regionInfo = document.querySelector("h1");
 regionInfo.innerText = regionInfo.innerText.replace(regionInfo.innerText[0], regionInfo.innerText[0].toUpperCase());
+
+/* Turn content screen Dynamic */
+
+var closeContentButton = document.createElement("div");
+closeContentButton.addEventListener("click", () => {
+    document.querySelectorAll(".content")[0].style.right = "-9999";
+})
+closeContentButton.style.cursor = "pointer";
+closeContentButton.innerText = "(X)";
+
+document.querySelectorAll(".content")[0].firstElementChild.appendChild(closeContentButton);
